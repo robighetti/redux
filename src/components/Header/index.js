fe0a3783236e26ff6e5fa42b42f312e28326eed9
robighetti <robighetti@gmail.com>
2019-07-23 12:54:34 -0300

    continuação redux

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,13 +1,25 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { MdShoppingBasket } from 'react-icons/md';
 
-import { Container } from './styles';
+import { Container, Cart } from './styles';
 
 import logo from '../../assets/images/logo.svg';
 
 export default function Header() {
   return (
     <Container>
-      <img src={logo} alt="Stack Shoes" />
+      <Link to="/">
+        <img src={logo} alt="Stack Shoes" />
+      </Link>
+
+      <Cart to="/cart">
+        <div>
+          <strong>Meu Carrinho</strong>
+          <span>Itens</span>
+        </div>
+        <MdShoppingBasket size={36} color="#fff" />
+      </Cart>
     </Container>
   );
 }
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,95 @@
 import React from 'react';
+import { MdAddShoppingCart } from 'react-icons/md';
 
-// import { Container } from './styles';
+import { ProductList } from './styles';
 
 export default function Home() {
-  return <div />;
+  return (
+    <ProductList>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-couro-olympikus-flashback/06/D22-1910-006/D22-1910-006_detalhe2.jpg?resize=326:*"
+          alt="Tênis"
+        />
+        <strong>Tenis Muito Legal</strong>
+        <span>R$ 129,90</span>
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="fff" /> 3
+          </div>
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-couro-olympikus-flashback/06/D22-1910-006/D22-1910-006_detalhe2.jpg?resize=326:*"
+          alt="Tênis"
+        />
+        <strong>Tenis Muito Legal</strong>
+        <span>R$ 129,90</span>
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="fff" /> 3
+          </div>
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-couro-olympikus-flashback/06/D22-1910-006/D22-1910-006_detalhe2.jpg?resize=326:*"
+          alt="Tênis"
+        />
+        <strong>Tenis Muito Legal</strong>
+        <span>R$ 129,90</span>
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="fff" /> 3
+          </div>
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-couro-olympikus-flashback/06/D22-1910-006/D22-1910-006_detalhe2.jpg?resize=326:*"
+          alt="Tênis"
+        />
+        <strong>Tenis Muito Legal</strong>
+        <span>R$ 129,90</span>
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="fff" /> 3
+          </div>
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-couro-olympikus-flashback/06/D22-1910-006/D22-1910-006_detalhe2.jpg?resize=326:*"
+          alt="Tênis"
+        />
+        <strong>Tenis Muito Legal</strong>
+        <span>R$ 129,90</span>
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="fff" /> 3
+          </div>
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-couro-olympikus-flashback/06/D22-1910-006/D22-1910-006_detalhe2.jpg?resize=326:*"
+          alt="Tênis"
+        />
+        <strong>Tenis Muito Legal</strong>
+        <span>R$ 129,90</span>
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" /> 3
+          </div>
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+    </ProductList>
+  );
 }
