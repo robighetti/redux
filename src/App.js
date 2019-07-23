8afef56b694e35a6e992bcdf7a11fac0f5ded148
robighetti <robighetti@gmail.com>
2019-07-23 11:41:53 -0300

    Aula Redux

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+
+import GlobalStyles from './styles/global';
+import Header from './components/Header';
+import Routes from './routes';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <BrowserRouter>
+        <Header />
+        <Routes />
+        <GlobalStyles />
+      </BrowserRouter>
     </div>
   );
 }
