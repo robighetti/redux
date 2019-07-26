e24febde7fc109122cf66c8f8959e473bf83b613
robighetti <robighetti@gmail.com>
2019-07-25 22:23:53 -0300

    finalização do modulo flux

--- src/store/index.js
+++ src/store/index.js
@@ -4,7 +4,14 @@ import createSagaMidleware from 'redux-saga';
 import rootReducer from './modules/rootReducer';
 import rootSaga from './modules/rootSaga';
 
-const sagaMidleware = createSagaMidleware();
+const sagaMonitor =
+  process.env.NODE_ENV === 'development'
+    ? console.tron.createSagaMonitor()
+    : null;
+
+const sagaMidleware = createSagaMidleware({
+  sagaMonitor,
+});
 
 const enhancer =
   process.env.NODE_ENV === 'development'
